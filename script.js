8c4619c83911855abe634c0016a8da06ad7339a5
MekkoW <150519980+MekkoW@users.noreply.github.com>
2023-11-11 17:30:54 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-function startTimer() {
+// script.js
+
+function startAgeTimer() {
   const birthdateInput = document.getElementById('birthdate');
+
+  // Store birthdate in local storage for access in the timer page.
+  localStorage.setItem('birthdate', birthdateInput.value);
+
+  // Redirect to the timer page.
+  window.location.href = 'index.html#timer';
+}
+
+function displayTimer() {
   const resultDiv = document.getElementById('result');
-  const calculatorDiv = document.getElementById('ageCalculator');
-
-  const birthdate = new Date(birthdateInput.value);
-  const currentDate = new Date();
-
-  calculatorDiv.style.display = 'none'; // Hide the calculator section
 
   function updateAge() {
+    const birthdate = new Date(localStorage.getItem('birthdate'));
+    const currentDate = new Date();
+
     const ageInMilliseconds = currentDate - birthdate;
     const ageInSeconds = ageInMilliseconds / 1000;
     const ageInMinutes = ageInSeconds / 60;
@@ -16,13 +24,22 @@ function startTimer() {
     const ageInDays = ageInHours / 24;
     const ageInYears = ageInDays / 365.25;
 
-    const formattedAge = new Intl.NumberFormat('en-US', { maximumFractionDigits: 6 }).format(ageInYears);
+    const formattedAge = new Intl.NumberFormat('en-US', { maximumFractionDigits: 9 }).format(ageInYears);
     resultDiv.innerHTML = `Your age is approximately ${formattedAge} years.`;
   }
 
   // Call the updateAge function immediately to display the initial age.
   updateAge();
 
-  // Set up an interval to update the age every 1000 milliseconds (1 second).
-  setInterval(updateAge, 1000);
+  // Set up an interval to update the age every 10 milliseconds.
+  setInterval(updateAge, 10);
+}
+
+function goBack() {
+  window.location.href = 'index.html';
+}
+
+// Check if the page should display the timer immediately.
+if (window.location.hash === '#timer') {
+  displayTimer();
 }
