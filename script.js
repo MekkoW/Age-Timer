905c074b784bb998dc7a4fb24fad0deae68564c5
MekkoW <150519980+MekkoW@users.noreply.github.com>
2023-11-11 16:38:13 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-function calculateAge() {
+function startTimer() {
   const birthdateInput = document.getElementById('birthdate');
+  const resultDiv = document.getElementById('result');
+  const calculatorDiv = document.getElementById('ageCalculator');
 
-  // Store birthdate in local storage for access in the timer page.
-  localStorage.setItem('birthdate', birthdateInput.value);
+  const birthdate = new Date(birthdateInput.value);
+  const currentDate = new Date();
 
-  // Redirect to the timer page.
-  window.location.href = 'timer.html';
+  calculatorDiv.style.display = 'none'; // Hide the calculator section
+
+  function updateAge() {
+    const ageInMilliseconds = currentDate - birthdate;
+    const ageInSeconds = ageInMilliseconds / 1000;
+    const ageInMinutes = ageInSeconds / 60;
+    const ageInHours = ageInMinutes / 60;
+    const ageInDays = ageInHours / 24;
+    const ageInYears = ageInDays / 365.25;
+
+    const formattedAge = new Intl.NumberFormat('en-US', { maximumFractionDigits: 9 }).format(ageInYears);
+    resultDiv.innerHTML = `Your age is approximately ${formattedAge} years.`;
+  }
+
+  // Call the updateAge function immediately to display the initial age.
+  updateAge();
+
+  // Set up an interval to update the age every 10 milliseconds.
+  setInterval(updateAge, 10);
+}
+
+function goBack() {
+  const calculatorDiv = document.getElementById('ageCalculator');
+  const resultDiv = document.getElementById('result');
+
+  calculatorDiv.style.display = 'block'; // Show the calculator section
+  resultDiv.innerHTML = ''; // Clear the result when going back
 }
