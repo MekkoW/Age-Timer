6c08ebd5be5890098b50e1c5835bb82203909745
MekkoW <150519980+MekkoW@users.noreply.github.com>
2023-11-11 15:36:35 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let birthday;
-    let timerInterval;
+document.addEventListener("DOMContentLoaded", function () {
+  let birthday;
+  let timerInterval;
 
-    function updateAgeTimer() {
-        const currentTime = new Date();
-        const ageInSeconds = (currentTime - birthday) / 1000;
-        const age = Math.floor(ageInSeconds / 31536000); // 1 year = 31536000 seconds
-        const decimalPart = Math.floor((ageInSeconds % 1) * 1000000000).toString().padStart(9, '0'); // Extract and format the decimal part
+  function updateAgeTimer() {
+    const currentTime = new Date();
+    const ageInSeconds = (currentTime - birthday) / 1000;
+    const age = Math.floor(ageInSeconds / 31536000); // 1 year = 31536000 seconds
+    const decimalPart = (ageInSeconds % 1).toFixed(9).substring(2);
 
-        // Update the timer display
-        document.getElementById("age-display").innerHTML = `Current Age: <span>${age}.${decimalPart}</span>`;
+    // Update the timer display
+    document.getElementById("age-display").innerHTML = `Current Age: <span>${age}.${decimalPart}</span>`;
+  }
+
+  function startTimer() {
+    const selectedDate = document.getElementById("birthday").value;
+    if (!selectedDate) {
+      alert("Please select your birthday first.");
+      return;
     }
 
-    function startTimer() {
-        const selectedDate = document.getElementById("birthday").value;
-        if (!selectedDate) {
-            alert("Please select your birthday first.");
-            return;
-        }
+    birthday = new Date(selectedDate);
+    updateAgeTimer(); // Initial update
+    timerInterval = setInterval(updateAgeTimer, 100);
+    document.getElementById("age-display").innerHTML = "Timer started!";
+  }
 
-        birthday = new Date(selectedDate);
-        updateAgeTimer(); // Initial update
-        timerInterval = setInterval(updateAgeTimer, 100);
-        document.getElementById("age-display").innerHTML = "Timer started!";
-    }
-
-    // Expose the startTimer function to the global scope
-    window.startTimer = startTimer;
+  // Expose the startTimer function to the global scope
+  window.startTimer = startTimer;
 });
