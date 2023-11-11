239d6ed3a0468798f2344493b32a3de06a5b2901
MekkoW <150519980+MekkoW@users.noreply.github.com>
2023-11-11 16:15:56 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 function calculateAge() {
   const birthdateInput = document.getElementById('birthdate');
-  const resultDiv = document.getElementById('result');
 
-  const birthdate = new Date(birthdateInput.value);
-  const currentDate = new Date();
+  // Store birthdate in local storage for access in the timer page.
+  localStorage.setItem('birthdate', birthdateInput.value);
 
-  const ageInMilliseconds = currentDate - birthdate;
-  const ageInSeconds = ageInMilliseconds / 1000;
-  const ageInMinutes = ageInSeconds / 60;
-  const ageInHours = ageInMinutes / 60;
-  const ageInDays = ageInHours / 24;
-  const ageInYears = ageInDays / 365.25;
-
-  const formattedAge = new Intl.NumberFormat('en-US', { maximumFractionDigits: 6 }).format(ageInYears);
-
-  resultDiv.innerHTML = `Your age is approximately ${formattedAge} years.`;
-
-  setInterval(() => {
-    const updatedAge = new Intl.NumberFormat('en-US', { maximumFractionDigits: 6 }).format(ageInYears + ((Date.now() - currentDate) / 1000 / 60 / 60 / 24 / 365.25));
-    resultDiv.innerHTML = `Your age is approximately ${updatedAge} years.`;
-  }, 1000);
+  // Redirect to the timer page.
+  window.location.href = 'timer.html';
 }
